d7436889577ebcd20fdff74cc5f0035312b54539
thomas mathew <thomasmathew365@gmail.com>
2021-06-01 16:04:33 +0530

    changed call to directly hit endpoint

--- hooks/useLocalCountriesData.js
+++ hooks/useLocalCountriesData.js
@@ -8,7 +8,7 @@ export default function useLocalCountriesData() {
   useEffect(() => {
     const localCountriesData = window.localStorage.getItem('countriesData');
     if (!localCountriesData) {
-      axios.get(`http://localhost:3000/api/all-countries`).then((response) => {
+      axios.get(`https://restcountries.eu/rest/v2/all`).then((response) => {
         window.localStorage.setItem(
           'countriesData',
           response && JSON.stringify(parseCountryData(response.data))
